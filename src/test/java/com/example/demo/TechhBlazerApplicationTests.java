335787b69e9977f8ef175b05ff9e16c99e6429f8
djfemz <oladejifemi00@gmail.com>
2025-01-14 12:58:12 +0100

    added userRegister

--- src/test/java/com/example/demo/TechhBlazerApplicationTests.java
+++ src/test/java/com/example/demo/TechhBlazerApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TechhBlazerApplicationTests {
 
@@ -11,4 +13,8 @@ class TechhBlazerApplicationTests {
 		System.out.println("Hello World");
 	}
 
+	@Test
+	void testUserRegister(){
+		assertTrue(true);
+	}
 }
